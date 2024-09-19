b014c0fd8068fd4f14bdc015df58b8df8c361948
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-19 15:09:21 +0700

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,4 @@
 def call(Map config = [:]){
- sh "echo Hello how are you to day ${config.name}. I am very happy to meet ${config.yourname}."
+ sh "echo Hello how are you to day ${config.name}. 
+ I am very happy to meet ${config.yourname}."
 }
