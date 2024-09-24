36c083a48b6d86b3cd142a216b09fc6a67d03042
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 06:15:17 +0700

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,9 +1,4 @@
-def call(Map config = [:]){
 
- sh "echo Hello how are you to day ${config.name}. \ 
- sh "echo Hello how are you to day ${config.name}. \
- I am very happy to meet ${config.yourname}."
-}
 
 def call(Closure<Void> body) {
     node('macos') {
