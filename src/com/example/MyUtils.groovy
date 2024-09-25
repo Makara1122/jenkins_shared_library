ba221bb04d14ab97f5c118b2ea175b0f745aa748
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 08:16:46 +0700

    Update MyUtils.groovy

--- src/com/example/MyUtils.groovy
+++ src/com/example/MyUtils.groovy
@@ -1,8 +1,7 @@
 package com.example
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
+
+def sayHello() {
+  echo 'Hello, World!'
 }
+
+return this
