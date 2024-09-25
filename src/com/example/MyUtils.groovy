9ed7cdab38ee84cc78932863c160a58c689792b6
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 10:23:50 +0700

    Update MyUtils.groovy

--- src/com/example/MyUtils.groovy
+++ src/com/example/MyUtils.groovy
@@ -3,8 +3,30 @@ package com.example
 import jenkins.model.Jenkins
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-class MyUtils {
-    static void sayHello(CpsScript script) {
-        script.echo 'Hello, World!'
+class Utils {
+    static void someStaticMethod(CpsScript script) {
+        script.echo 'Executing someStaticMethod'
     }
+}
+
+class Helper {
+    private CpsScript script
+    private String text
+
+    Helper(CpsScript script, String text) {
+        this.script = script
+        this.text = text
+    }
+
+    void prepare() {
+        script.echo "Preparing with text: ${text}"
+    }
+
+    int count() {
+        return text.length()
+    }
+}
+
+class Constants {
+    static final String SOME_TEXT = "Hello from Constants"
 }
