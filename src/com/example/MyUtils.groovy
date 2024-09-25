f4b68e4fb5a52254ad4f85efbf52526bda986f64
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 08:35:47 +0700

    Update MyUtils.groovy

--- src/com/example/MyUtils.groovy
+++ src/com/example/MyUtils.groovy
@@ -2,15 +2,14 @@ package com.example
 
 class MyUtils {
   def sayHello() {
-  echo 'Hello, World!'
-}
+    echo 'Hello, World!'
+  }
+
   def sayHi() {
     echo 'Hi Makara*'
   }
 
-  def greeting(Map config = [:]){
-    echo 'hello makara, ${config.name}'
+  def greeting(Map config = [:]) {
+    echo "hello makara, ${config.name}"  // Use double quotes for string interpolation
   }
 }
-
-return this
