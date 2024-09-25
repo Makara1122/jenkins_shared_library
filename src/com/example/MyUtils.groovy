eab40a21345b043de0c9ae0b71a9957fc16e0e05
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 10:01:49 +0700

    Update MyUtils.groovy

--- src/com/example/MyUtils.groovy
+++ src/com/example/MyUtils.groovy
@@ -2,20 +2,13 @@ package com.example
 
 
 
+
+
 class MyUtils {
-    def steps
-
-    MyUtils(steps) {
-        this.steps = steps
-    }
-
     static void sayHello() {
         echo 'Hello, World!'
     }
-
-    def greeting(Map config = [:]) {
-        steps.echo "here it is ${config.name}"
-    }
 }
+
 
 
