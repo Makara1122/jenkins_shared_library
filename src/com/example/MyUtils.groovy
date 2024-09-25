81df60d61f1210d61f9035a2002704f2bb3015ef
Makara <148768781+Makara1122@users.noreply.github.com>
2024-09-25 08:37:12 +0700

    Update MyUtils.groovy

--- src/com/example/MyUtils.groovy
+++ src/com/example/MyUtils.groovy
@@ -5,11 +5,5 @@ class MyUtils {
     echo 'Hello, World!'
   }
 
-  def sayHi() {
-    echo 'Hi Makara*'
-  }
-
-  def greeting(Map config = [:]) {
-    echo "hello makara, ${config.name}"  // Use double quotes for string interpolation
-  }
+  
 }
